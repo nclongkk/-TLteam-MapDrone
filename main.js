370305b3800b833ea69b7ba9e1af91e0d82b9d47
nclongkk <nclongkk@gmail.com>
2020-12-29 15:46:45 +0700

    update middle point

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 window.onload = init;
 
 function init() {
+  let date = getDate();
+  $("#Date").text(date); 
   var center = ol.proj.transform([108.15219145246553, 16.076152616052678], 'EPSG:4326', 'EPSG:3857'); //khởi tạo vị trí ban đầu của map
   var view = new ol.View({
     center: center,
@@ -24,24 +26,6 @@ function init() {
     controls: [new ol.control.FullScreen(), new ol.control.Zoom()]
   });
 
-  function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
-    var R = 6371; // Radius of the earth in km
-    var dLat = deg2rad(lat2 - lat1);  // deg2rad below
-    var dLon = deg2rad(lon2 - lon1);
-    var a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-      Math.sin(dLon / 2) * Math.sin(dLon / 2)
-      ;
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c; // Distance in km
-    return d;
-  }
-
-  function deg2rad(deg) {
-    return deg * (Math.PI / 180)
-  }
-
   var distance = 0;
   var data = [];
   map.on('click', function (e) {
@@ -56,58 +40,91 @@ function init() {
     //tính khoảng cách 2 điểm
     distance = distance + getDistanceFromLatLonInKm(data[data.length-1].Lat,data[data.length-1].Lon,data[data.length-2].Lat,data[data.length-2].Lon)
     distance = roundToTwo(distance)// làm tròn khoảng cách lên 2 chữ số
-    $("#distance").text("Distance: "  + distance + "km"); //jquery display HTML
+    $("#distance").text("Lộ trình bay: "  + distance + "km"); //jquery display HTML
   })
-  function insertMiddlePoint(coor1,coor2,k){
-    let distance = Math.sqrt(Math.pow((coor2.Lon-coor1.Lon),2) +Math.pow((coor2.Lat-coor1.Lat),2))
-    let n = Math.floor(distance/k)
-    for (i = 1; i <= n; i++) {
-      insertelement(coor1,coor2,i*k)
-    }
-    
-  }
-  function insertelement(coor1,coor2,k){
-
-    let x2 = coor2.Lon - coor1.Lon;
-    let y2 = coor2.Lat - coor1.Lat;
-    let delta = (1 + Math.pow((y2/x2),2))*(k*k)
-    let xi = ( Math.sign(x2)*Math.sqrt(delta))/ (1+ Math.pow((y2/x2),2))
-    let yi = (xi*y2/x2)
-    var coorInsert = ({
-      Lon : xi + coor1.Lon,
-      Lat : yi + coor1.Lat
-    })
-    addPointGeom(coorInsert)
-  }
-
-
-
-  //hàm làm tròn đến 2 chữ số thập phân
-  function roundToTwo(num) {    
-    return +(Math.round(num + "e+2")  + "e-2");
-  }
-  
-  //hàm thêm marker được chọn vào map
-  function addPointGeom(item) {
-      var longitude = item.Lon,
-        latitude = item.Lat,
-        iconFeature = new ol.Feature({
-          geometry: new ol.geom.Point(ol.proj.transform([longitude, latitude], 'EPSG:4326',
-            'EPSG:3857')),
-        }),
-        iconStyle = new ol.style.Style({
-          image: new ol.style.Circle({
-            radius: 4,
-            stroke: new ol.style.Stroke({
-              color: 'blue'
-            }),
-            fill: new ol.style.Fill({
-              color: [57, 228, 193, 0.84]
-            }),
-          })
-        });
-      iconFeature.setStyle(iconStyle);
-      straitSource.addFeature(iconFeature);
-  }
 
 }
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+  var dLon = deg2rad(lon2 - lon1);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180)
+}
+
+//chèn các marker ở giữa 2 tọa độ đã chọn vào map
+function insertMiddlePoint(coor1,coor2,k){
+  let distance = Math.sqrt(Math.pow((coor2.Lon-coor1.Lon),2) +Math.pow((coor2.Lat-coor1.Lat),2))
+  let n = Math.floor(distance/k)
+  for (i = 1; i <= n; i++) {
+    insertelement(coor1,coor2,i*k)
+  }
+}
+function insertelement(coor1,coor2,k){
+
+  let x2 = coor2.Lon - coor1.Lon;
+  let y2 = coor2.Lat - coor1.Lat;
+  let delta = (1 + Math.pow((y2/x2),2))*(k*k)
+  let xi = ( Math.sign(x2)*Math.sqrt(delta))/ (1+ Math.pow((y2/x2),2))
+  let yi = (xi*y2/x2)
+  var coorInsert = ({
+    Lon : xi + coor1.Lon,
+    Lat : yi + coor1.Lat
+  })
+  addPointGeom(coorInsert)
+}
+
+
+
+//hàm làm tròn đến 2 chữ số thập phân
+function roundToTwo(num) {    
+  return +(Math.round(num + "e+2")  + "e-2");
+}
+
+//hàm thêm marker được chọn vào map
+function addPointGeom(item) {
+    var longitude = item.Lon,
+      latitude = item.Lat,
+      iconFeature = new ol.Feature({
+        geometry: new ol.geom.Point(ol.proj.transform([longitude, latitude], 'EPSG:4326',
+          'EPSG:3857')),
+      }),
+      iconStyle = new ol.style.Style({
+        image: new ol.style.Circle({
+          radius: 4,
+          stroke: new ol.style.Stroke({
+            color: 'blue'
+          }),
+          fill: new ol.style.Fill({
+            color: [57, 228, 193, 0.84]
+          }),
+        })
+      });
+    iconFeature.setStyle(iconStyle);
+    straitSource.addFeature(iconFeature);
+}
+//Hàm ngày giờ
+function AddZero(num) {
+  return (num >= 0 && num < 10) ? "0" + num : num + "";
+}
+
+function getDate() {
+  var now = new Date();
+  var strDateTime = [[AddZero(now.getDate()), 
+      AddZero(now.getMonth() + 1), 
+      now.getFullYear()].join("/"), 
+      [AddZero(now.getHours()), 
+      AddZero(now.getMinutes())].join(":"), 
+      now.getHours() >= 12 ? "PM" : "AM"].join(" ");
+  return strDateTime;
+};
